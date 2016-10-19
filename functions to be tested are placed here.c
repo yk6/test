a20585862e911ff04215232c9eeb8a09299809a3
yk6 <grantex5@gmail.com>
2016-10-20 02:07:45 +0800

    rgb blue to be debugged

--- functions to be tested are placed here.c
+++ functions to be tested are placed here.c
@@ -1,1 +1,33 @@
+//  ways to set rgb
 
+
+
+/**********************************************
+	set up GPIO
+
+	P2.0 is red
+	P0.26 is blue 
+	P2.1 is green
+
+	then set direction  
+
+	then 
+	GPIO_ClearValue
+	GPIO_SetValue
+	GPIO_ReadValue
+
+
+
+	or use rgb library     0x01 is red
+						   0x02 is blue
+						   0x04 is green
+
+	current problem is can control rgb red to blink at desire freq but the blue rgb will always juz light up 
+
+	and stop changing.
+
+	tested with manually inputting flag to set value can work,   but dk why cant make it blink
+
+
+
+*************************************************/

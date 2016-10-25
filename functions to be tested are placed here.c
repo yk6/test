638c24583c92d2d0b266df63b00afc75a603c6f2
yk6 <grantex5@gmail.com>
2016-10-25 12:51:21 +0800

    added timer0

--- functions to be tested are placed here.c
+++ functions to be tested are placed here.c
@@ -21,3 +21,51 @@ uint32_t t3 = 0;
 	then find out t3 is what   then follow this formula 
 
 	T = t3 / 10 - 273.15 ;
+
+/*********************************************************************************************
+
+			the top one gg cuz cant make that accurate clock 
+
+*********************************************************************************************/
+
+//--------------------------------------------------------------------------------------------
+	// make faster SPI for oled
+
+	SSP_ConfigStructInit(&SSP_ConfigStruct);
+	SSP_ConfigStruct.ClockRate = 8000000;
+	
+
+//--------------------------------------------------------------------------------------------
+
+void TimerInit(void) {
+	TIM_TIMERCFG_Type timer;
+	TIM_MATCHCFG_Type match;
+	
+	timer.PrescaleOption = TIM_PRESCALE_USVAL;
+	timer.PrescaleValue = 1;
+	TIM_Init(LPC_TIM0, TIM_TIMER_MODE, &timer);
+	
+	match.ExtMatchOutputType = 0;
+	match.IntOnMatch = ENABLE;
+	match.MatchChannel = 0;
+	match.MatchValue = 100;
+	match.ResetOnMatch = ENABLE;
+	match.StopOnMatch = DISABLE;
+	TIM_ConfigMatch(LPC_TIM0, &match);
+	
+	TIM_Cmd(LPC_TIM0, ENABLE);
+	TIM_ResetCounter(LPC_TIM0);
+}
+
+
+void TIMER0_IRQHandler (void) {
+	usTick++;
+	LPC_TIM0 -> IR |= 0x01;
+}
+
+int main (void) {
+	NVIC_SetPriority(TIMER0_IRQn,0);
+	NVIC_ClearPendingIRQ(TIMER0_IRQn);
+	NVIC_EnableIRQ(TIMER0_IRQn);
+
+}
